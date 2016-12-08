8db0d2cef779d6afed9e5fe48dfcba5fff003d8c
Rhys Sharrem <rhys.sharrem@gmail.com>
2016-12-08 14:12:33 +0000

    Formatting

--- src/main/scala/ReferentialTransparency2.scala
+++ src/main/scala/ReferentialTransparency2.scala
@@ -2,14 +2,13 @@ import org.joda.time.DateTime
 
 object ScheduleBuilder2 {
 
-  def buildProgrammeSchedule(programmes: Seq[Programme]): Seq[Programme] = {
-    programmes.sortBy(_.airDate)
+  def buildProgrammeSchedule(programmes:
+                             List[Programme]):
+  List[Programme] = {
+    programmes
+      .sortBy(_.airDate)
   }
 
-}
-
-class ProgrammeLookupService {
-  def getProgrammes: List[Programme] = ???
 }
 
 case class Programme(name: String, airDate: DateTime)
